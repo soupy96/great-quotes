e926245a77557d10df76c300cde4c4871cf25741
Michael Campbell <michael.campbell14@gmail.com>
2022-10-24 18:03:51 -0400

    dispalying the quote depending on which quote is picked

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,20 @@
+import QuoteList from '../components/quotes/QuoteList';
+
+const DUMMY_QUOTES = [
+  {
+    id: 'q1',
+    author: 'Michael',
+    text: 'Learning React is fun!',
+  },
+  {
+    id: 'q2',
+    author: 'Robyn',
+    text: 'Learning React is great!',
+  },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes page!</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
--- src/pages/SingleQuote.js
+++ src/pages/SingleQuote.js
@@ -1,13 +1,33 @@
 import { Route, useParams } from 'react-router-dom';
 
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
+
+const DUMMY_QUOTES = [
+  {
+    id: 'q1',
+    author: 'Michael',
+    text: 'Learning React is fun!',
+  },
+  {
+    id: 'q2',
+    author: 'Robyn',
+    text: 'Learning React is great!',
+  },
+];
 
 const SingleQuotes = () => {
   const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found.</p>;
+  }
+
   return (
     <section>
-      <h1>Single Quotes page!</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
